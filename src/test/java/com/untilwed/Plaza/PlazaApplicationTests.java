69e97ed0140c6b1ba34ab05b82ffca8da9ca7a7b
Jhin Woo <muojeso90@gmail.com>
2023-08-03 11:33:55 +0900

    first commit

---
+++ src/test/java/com/untilwed/Plaza/PlazaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.untilwed.Plaza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlazaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
