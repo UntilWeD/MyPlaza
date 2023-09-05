d55ef15a7395216dca8fd8c6112bb2db2446221e
Jhin Woo <muojeso90@gmail.com>
2023-09-05 09:14:50 +0900

    내부에러컨트롤러제어 준비

---
+++ src/main/java/com/untilwed/plaza/error/ErrorController.java
@@ -0,0 +1,10 @@
+package com.untilwed.plaza.error;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+
+@Slf4j
+@Controller
+public class ErrorController {
+
+}
