c7e0157b96a0401df0c01ea770129899a1bb3e2b
Jhin Woo <muojeso90@gmail.com>
2023-09-06 09:11:48 +0900

    에러페이지, 로그인터셉터 생성

--- src/main/java/com/untilwed/plaza/error/ErrorController.java
+++ src/main/java/com/untilwed/plaza/error/ErrorController.java
@@ -1,10 +1,24 @@
 package com.untilwed.plaza.error;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.io.IOException;
 
 @Slf4j
 @Controller
 public class ErrorController {
 
+    @GetMapping("/error-404")
+    public void error404(HttpServletResponse response) throws IOException{
+        response.sendError(404, "404 오류");
+    }
+
+    @GetMapping("/error-500")
+    public void error500(HttpServletResponse response) throws IOException{
+        response.sendError(500, "500 에러");
+    }
 }
